a6488a3ab43a4f07a50287b77e974b9ffcae620d
Mateo Villada <mvillada087@gmail.com>
2026-06-18 14:05:45 -0500

    Create source not found except

---
+++ src/main/java/net/javaguides/employee_backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package net.javaguides.employee_backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
